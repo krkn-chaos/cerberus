7e014bd70c532101cb4de841337bb0471d60df7c
Yashashree Suresh <ysuresh@redhat.com>
2020-04-06 11:12:41 +0530

    Added support to ensure functionality isn't affected on new commit
    This commit:
    - Ensures functionality of cerberus is not affected/broken by a
      new commit.
    - Adds a functional test to ensure non-daemon mode of cerberus is
      not affected by a new commit.
    - Adds a functional test to ensure the functionality of cerberus
      to collect detailed logs, events and metrics of failed components
      is not affected.
    - Adds a functional test to ensure the slack integration feature is
      not affected by a new commit.

---
+++ tests/test_daemon_disabled.sh
@@ -0,0 +1,14 @@
+set -xeEo pipefail
+
+source tests/common.sh
+
+trap error ERR
+trap finish EXIT
+
+function funtional_test_daemon_disabled {
+    sed -i '/^\([[:space:]]*iterations: *\).*/s//\15/;/^\([[:space:]]*sleep_time: *\).*/s//\12/;/^\([[:space:]]*daemon_mode: *\).*/s//\1False/;' config/config.yaml
+    python3 start_cerberus.py -c config/config.yaml
+    echo "${test_name} test: Success"
+}
+
+funtional_test_daemon_disabled
