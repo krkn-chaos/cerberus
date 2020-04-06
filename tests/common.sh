ba2b043fcb50d69c1a17c3f4b8d9a6cbb9cbf6d3
Yashashree Suresh <ysuresh@redhat.com>
2020-04-06 11:12:41 +0530

    Added support to ensure functionality isn't affected on new commit
    This commit:
    - Ensures functionality of cerberus is not affected/broken by a
      new commit.
    - Adds a functional test to ensure non-daemon mode of cerberus is
      not affected by a new commit.
    - Adds a functional test to ensure the functionality of cerberus
      to collect detailed logs, events and metrics of failed components
      is not affected.
    - Adds a functional test to ensure the slack integration feature is
      not affected by a new commit.

---
+++ tests/common.sh
@@ -0,0 +1,23 @@
+ERRORED=false
+
+function finish {
+    if [ $? -eq 1 ] && [ $ERRORED != "true" ]
+    then
+        error
+    fi
+}
+
+function error {
+    echo "Error caught."
+    ERRORED=true
+}
+
+function create_and_delete_pod {
+    for ((i=1;i<5;i++));
+    do
+        oc apply -f tests/test-configs/hello_openshift_pod.yaml -n test-namespace
+        kubectl wait --for=condition=Ready pod/hello-pod -n test-namespace
+        sleep 2s
+        oc delete pods hello-pod -n test-namespace 
+    done
+}
