0371dadb947ec3be195587e28863ee88832d8125
Yashashree Suresh <ysuresh@redhat.com>
2020-06-15 13:08:11 +0530

    Refactored to improve readability

--- cerberus/prometheus/client.py
+++ cerberus/prometheus/client.py
@@ -1,25 +1,39 @@
+import urllib3
 import logging
 import prometheus_api_client
-import urllib3
+import cerberus.invoke.command as runcommand
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 # Initialize the client
-def initialize_prom_client(url, token):
+def initialize_prom_client(distribution, prometheus_url, prometheus_bearer_token):
     global prom_cli
-    bearer = "Bearer " + token
-    headers = {'Authorization': bearer}
-    try:
-        prom_cli = prometheus_api_client.PrometheusConnect(url=url,
-                                                           headers=headers,
-                                                           disable_ssl=True)
-    except Exception as e:
-        logging.error("Not able to initialize the client %s" % e)
+    if distribution == "openshift" and not prometheus_url:
+        url = runcommand.invoke(r"""oc get routes -n openshift-monitoring -o=jsonpath='{.items[?(@.metadata.name=="prometheus-k8s")].spec.host}'""") # noqa
+        prometheus_url = "https://" + url
+    if distribution == "openshift" and not prometheus_bearer_token:
+        prometheus_bearer_token = runcommand.invoke("oc -n openshift-monitoring "
+                                                    "sa get-token prometheus-k8s")
+    if prometheus_url and prometheus_bearer_token:
+        bearer = "Bearer " + prometheus_bearer_token
+        headers = {'Authorization': bearer}
+        try:
+            prom_cli = prometheus_api_client.PrometheusConnect(url=prometheus_url,
+                                                               headers=headers,
+                                                               disable_ssl=True)
+        except Exception as e:
+            logging.error("Not able to initialize the client %s" % e)
+    else:
+        prom_cli = None
 
 
-# Capture the metrics
-def get_metrics(query):
-    try:
-        return prom_cli.custom_query(query=query, params=None)
-    except Exception as e:
-        logging.error("Failed to get the metrics: %s" % e)
+# Process custom prometheus query
+def process_prom_query(query):
+    if prom_cli:
+        try:
+            return prom_cli.custom_query(query=query, params=None)
+        except Exception as e:
+            logging.error("Failed to get the metrics: %s" % e)
+    else:
+        logging.info("Skipping the prometheus query as the prometheus client couldn't "
+                     "be initilized\n")
