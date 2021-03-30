c20e728afb26f674661d0007a117e25fef8b867e
Harshith u <44830597+Harshith-umesh@users.noreply.github.com>
2021-03-30 18:12:43 +0530

    Update master_test.sh

--- CI/master_test.sh
+++ CI/master_test.sh
@@ -2,8 +2,7 @@
 
 mkdir master_gold_dir
 cd master_gold_dir
-git clone https://github.com/Harshith-umesh/cerberus.git
-#git clone https://github.com/cloud-bulldozer/cerberus.git
+git clone https://github.com/cloud-bulldozer/cerberus.git
 echo $PWD
 cd cerberus
 ./CI/tests/test_daemon_disabled.sh
