9edd510269b34687c28cb8476dbddf2d559ba448
amitsagtani97 <asagtani06@gmail.com>
2021-05-07 07:26:21 +0530

    fixed unbound variable error

--- cerberus/invoke/command.py
+++ cerberus/invoke/command.py
@@ -1,12 +1,14 @@
 import subprocess
 import logging
-
+import sys
 
 # Invokes a given command and returns the stdout
 def invoke(command):
+    output = ""
     try:
         output = subprocess.check_output(command, shell=True,
                                          universal_newlines=True)
     except Exception:
         logging.error("Failed to run %s" % (command))
+        sys.exit(1)
     return output
