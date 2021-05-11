a77601be527f7434b6abad1ca2de49560a6f9efc
ratsuf <ratsuf@gmail.com>
2021-05-11 16:56:05 +0400

    Fix for getting optional information about openshift cluster version. (#137)
    * Fix for getting optional information about openshift cluster version.
    
    * Rephrased comment after rebase

--- cerberus/invoke/command.py
+++ cerberus/invoke/command.py
@@ -3,7 +3,8 @@ import logging
 import sys
 
 
-# Invokes a given command and returns the stdout
+# Invokes a given command and returns the stdout.
+# Will stop Cerberus execution with exit code 1.
 def invoke(command):
     output = ""
     try:
@@ -13,3 +14,18 @@ def invoke(command):
         logging.error("Failed to run %s" % (command))
         sys.exit(1)
     return output
+
+
+# Invokes a given command and returns the stdout.
+# In case of exception, returns message about the impossibility to execute the command instead of stdout.
+# It won't stop Cerberus execution but doesn't guarantee that command returns expected stdout.
+def optional_invoke(command):
+    try:
+        optional_output = subprocess.check_output(command, shell=True,
+                                                  universal_newlines=True)
+    except Exception:
+        optional_output = "Result is absent."
+        logging.info(
+            "Optional command '%s' can't be executed, but it's not a problem at all. We can continue." % command)
+
+    return optional_output
