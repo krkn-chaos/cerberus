7eb7e6ac3b6f3d41e01d9255f96669defe073d64
Paige Rubendall <prubenda@redhat.com>
2023-06-26 13:29:34 -0400

    wrong variable

--- start_cerberus.py
+++ start_cerberus.py
@@ -568,7 +568,7 @@ if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
-        handlers=[logging.FileHandler(options.report, mode="w"), logging.StreamHandler()],
+        handlers=[logging.FileHandler(options.output, mode="w"), logging.StreamHandler()],
     )
     if options.cfg is None:
         logging.error("Please check if you have passed the config")
