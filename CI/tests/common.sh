cf88105ca6338666684a74e67d77e174fa916d6b
Paige Patton <prubenda@redhat.com>
2025-10-23 12:55:59 -0400

    using kubectl
    Signed-off-by: Paige Patton <prubenda@redhat.com>

--- CI/tests/common.sh
+++ CI/tests/common.sh
@@ -15,9 +15,9 @@ function error {
 function create_and_delete_pod {
     for ((i=1;i<5;i++));
     do
-        oc apply -f CI/tests/test-configs/hello_openshift_pod.yaml -n test-namespace
+        kubectl apply -f CI/tests/test-configs/hello_openshift_pod.yaml -n test-namespace
         kubectl wait --for=condition=Ready pod/hello-pod -n test-namespace
         sleep 2s
-        oc delete pods hello-pod -n test-namespace
+        kubectl delete pods hello-pod -n test-namespace
     done
 }
